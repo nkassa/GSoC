722e4bb5e370cd73a768354d3eaf004669932e40
natan kassa <nkassa@usc.edu>
2025-04-07 02:42:42 -0700

    counter

---
+++ ratatui-counter-app/src/main.rs
@@ -0,0 +1,25 @@
+use std::io;
+
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
+use ratatui::{
+    DefaultTerminal, Frame,
+    style::Stylize,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+    symbols::border,
+    buffer::Buffer,
+    layout::Rect,
+};
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let app_result = App::default().run(&mut terminal);
+    ratatui::restore();
+    app_result
+}
+
+#[derive(Debug, Default)]
+pub struct App {
+    counter: u8,
+    exit: bool,
+}
